e037af105e28e261893deb4eb3d48e9cb9f240ea
Seth Neill <seth.neill@gmail.com>
2013-04-05 15:09:37 -0700

    Initial commit with added files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilmGraph::Application.config.secret_token = '8f8450cdd33e452152181af1bc5800d3862662e94549332d5f07c115d626bb2feebf8ac4309b811c0ca45d6f0f0d676f182412f9432da969dbc2a2e1115d4ea3'
